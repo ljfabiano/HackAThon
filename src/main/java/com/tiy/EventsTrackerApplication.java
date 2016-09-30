6c40c229e158fa618995256b129dabf71522bcb5
Joe Fabiano <jfabiano@gmail.com>
2016-09-30 07:46:19 -0400

    Initial Commit

---
+++ src/main/java/com/tiy/EventsTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.tiy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsTrackerApplication.class, args);
+	}
+}
